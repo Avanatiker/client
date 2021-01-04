04604cb2961704e97385d5041c2558bf1ed7869c
theredstoner <huddy987@yahoo.com>
2021-01-03 18:58:03 -0700

    Fix searchShulker not moving shulkers to hotbar
    If there was no shulker in the hotbar, shulkerBoxId stays at 0 and
    never gets set to a shulker ID. Correctly search the inventory to set
    the shulkerBoxId. This fixes moving shulkers to the hotbar

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/AutoObsidian.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/AutoObsidian.kt
@@ -372,21 +372,43 @@ object AutoObsidian : Module() {
         }
     }
 
+    /**
+     * @return The id of a shulker found in the hotbar, else returns -1
+     */
+    private fun getShulkerIdInHotbar(): Int {
+        for (shulkerId in 219..234) {
+            if (InventoryUtils.getSlotsHotbar(shulkerId) != null) return shulkerId
+        }
+        return -1
+    }
+
+    /**
+     * @return the id of a shulker found in a non-hotbar slot, else returns -1
+     */
+    private fun getShulkerIdNotInHotBar(): Int {
+        for (shulkerId in 219..234) {
+            if (InventoryUtils.getSlotsNoHotbar(shulkerId) != null) return shulkerId
+        }
+        return -1
+    }
+
     private fun placeShulker(pos: BlockPos) {
-        if (InventoryUtils.getSlotsHotbar(shulkerBoxId) == null && InventoryUtils.getSlotsNoHotbar(shulkerBoxId) != null) {
-            InventoryUtils.moveToHotbar(shulkerBoxId, Items.DIAMOND_PICKAXE.id)
+        val shulkerIdNotInHotbar = getShulkerIdNotInHotBar()
+        if (getShulkerIdInHotbar() == -1 && getShulkerIdNotInHotBar() != -1) {
+            InventoryUtils.moveToHotbar(shulkerIdNotInHotbar, Items.DIAMOND_PICKAXE.id)
+            shulkerBoxId = shulkerIdNotInHotbar
         } else {
-            for (i in 219..234) {
-                if (InventoryUtils.getSlotsHotbar(i) == null) {
-                    if (i == 234) {
+            for (shulkerId in 219..234) {
+                if (InventoryUtils.getSlotsHotbar(shulkerId) == null) {
+                    if (shulkerId == 234) {
                         sendChatMessage("$chatName No shulker box was found in hotbar, disabling.")
                         mc.soundHandler.playSound(PositionedSoundRecord.getRecord(SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f))
                         disable()
                     }
                     continue
                 }
-                shulkerBoxId = i
-                InventoryUtils.swapSlotToItem(i)
+                shulkerBoxId = shulkerId
+                InventoryUtils.swapSlotToItem(shulkerId)
                 break
             }
         }
